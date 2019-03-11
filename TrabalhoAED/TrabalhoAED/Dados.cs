dc394e0db4bffd25f570b16a7b8fbe03fcd91c60
Marcos Almeida <monitoria.puccontagem@gmail.com>
2019-03-11 19:53:25 -0300

    Classe criada

---
+++ TrabalhoAED/TrabalhoAED/Dados.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoAED
+{
+    class Dados
+    {
+        public int room_id { get; set; }
+        public int host_id { get; set; }
+        public string room_type { get; set; }
+        public string country { get; set; }
+        public string city { get; set; }
+        public string neighborhood { get; set; }
+        public int reviews { get; set; }
+        public double overall_satisfaction { get; set; }
+        public int accommodates { get; set; }
+        public double bedrooms { get; set; }
+        public double price { get; set; }
+        public string property_type { get; set; }
+    }
+}
